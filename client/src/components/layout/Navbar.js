bee8495a71b93f4a9e7b82cd49b33de92db31e89
rickybag <rickybag99@gmail.com>
2020-03-03 19:58:02 +0100

    added navbar, home & about

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { NavLink } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+      <div className="container">
+        <div className="navbar-brand">
+          <h3>
+            <i className={icon} /> {title}
+          </h3>
+        </div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <NavLink exact className="nav-link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt"
+};
+
+export default Navbar;
